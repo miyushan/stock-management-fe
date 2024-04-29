e6d24f9afb668325ba6753650f16ea0424b2da08
miyushan <miyushanrodrigo@gmail.com>
2024-04-29 12:05:47 +0530

    Dashboard view added

--- src/components/Auth/LogIn.vue
+++ src/components/Auth/LogIn.vue
@@ -1,9 +1,63 @@
 <template>
-  <div>LogIn</div>
+  <div class="min-h-screen flex items-center content-center">
+    <Card class="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-2xl"> Login </CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="grid gap-4">
+          <div class="grid gap-2">
+            <Label for="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div class="grid gap-2">
+            <div class="flex items-center">
+              <Label for="password">Password</Label>
+              <!-- <a href="#" class="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </a> -->
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" class="mt-2 w-full"> Login </Button>
+          <Button variant="outline" class="w-full"> Create Account </Button>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
 </template>
 
 <script>
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default {
   name: "LogIn",
+  components: {
+    Button,
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+    Input,
+    Label,
+  },
 };
 </script>
--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -1,9 +1,92 @@
 <template>
-  <div>Register</div>
+  <div class="min-h-screen flex items-center content-center">
+    <Card class="mx-auto w-[600px]">
+      <CardHeader>
+        <CardTitle class="text-xl"> Register </CardTitle>
+        <CardDescription>
+          Enter your information to create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="grid gap-4">
+          <div class="grid grid-cols-2 gap-4">
+            <div class="grid gap-2">
+              <Label for="first-name">First name</Label>
+              <Input id="first-name" placeholder="Max" required />
+            </div>
+            <div class="grid gap-2">
+              <Label for="last-name">Last name</Label>
+              <Input id="last-name" placeholder="Robinson" required />
+            </div>
+          </div>
+
+          <div class="grid grid-cols-2 gap-4">
+            <div class="grid gap-2">
+              <Label for="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div class="grid gap-2">
+              <Label for="phone">Mobile</Label>
+              <Input id="phone" type="tel" required />
+            </div>
+          </div>
+          <div class="grid grid-cols-2 gap-4">
+            <div class="grid gap-2">
+              <Label for="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+          </div>
+          <div class="grid grid-cols-2 gap-4">
+            <div class="grid gap-2">
+              <Label for="address1">Address 1</Label>
+              <Input id="address1" type="text" required />
+            </div>
+            <div class="grid gap-2">
+              <Label for="address2">Address 2</Label>
+              <Input id="address2" type="text" required />
+            </div>
+            <div class="grid gap-2">
+              <Label for="address3">Address 3</Label>
+              <Input id="address3" type="text" required />
+            </div>
+          </div>
+
+          <Button type="submit" class="mt-2 w-full"> Create an account </Button>
+          <!-- <Button variant="outline" class="w-full"> Sign In </Button> -->
+        </div>
+      </CardContent>
+    </Card>
+  </div>
 </template>
 
 <script>
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default {
   name: "Register",
+  components: {
+    Button,
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+    Input,
+    Label,
+  },
 };
 </script>
